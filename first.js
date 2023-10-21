8d85b357cbcc2c2d6ba566cf6ee5d1e0bde28c24
Aaditya Jagdale <aadityajagdale.21@gmail.com>
2023-10-21 23:24:33 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,15 @@
+const name = 'Max';
+let age = 28;
+const hasHob = true;
+
+age = 28
+
+const summeizeUsers = (username, userAge, userHob) => {
+    
+    return (
+    'Name is ' + username + ' age is ' + userAge + ' user has hobbies '+ userHob
+    );
+}
+
+const add = (a, b) => a+b;
+console.log(summeizeUsers(name , age, hasHob));
---
+++ play.js
@@ -0,0 +1,31 @@
+const person = {
+    name: 'Mike',
+    age: 25,
+    greet() {
+        console.log('Hi, I am ' + this.name);
+    } 
+}
+
+const  printName = (person) => {
+    console.log(person.name);
+};
+const  printsecName = ({name}) => {
+    console.log(person.name);
+};
+
+printName(person);
+printsecName(person);
+
+// const hobbies = ['Gym', 'Sex'];
+
+// /* for(let hobb of hobbies){
+//     console.log(hobb); 
+// } */
+
+// const cpy = [...hobbies];
+
+// console.log(cpy);
+// console.log(hobbies.map(hobby => 'hobby:' + hobby));
+
+// const toArray = ( ...args) => args;
+// console.log(toArray(1,2,3,4));
